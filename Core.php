98fa3593389e091de7dfa01a90a0bdb2d0414f9f
Eric Hocking <xaoseric@users.noreply.github.com>
2014-12-16 11:34:08 -0500

    Create Core.php

---
+++ Core.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name: ZippySites
+ * Plugin URI: http://plugin.pagezippy.com
+ * Description: Allows for management of paid and free packages for wordpress multisite installs
+ * Version: 1.0.0
+ * Author: XAOS Interactive
+ * Author URI: http://xaos-ia.com
+ * Network: true
+ * License: MIT
+ */
+ 
+namespace ZippySites;
+
+class Core {
+
+  public static function version() 
+  { 
+    return '1.0.0-alpha'; 
+  } 
+  
+}
