9800fc90da4c060dda73dbc5eb4f955d40e0da2e
Eric Hocking <xaoseric@users.noreply.github.com>
2014-12-16 13:34:43 -0500

    Update Core.php

--- Core.php
+++ Core.php
@@ -13,11 +13,31 @@
  
 namespace ZippySites;
 
-class Core {
+use ZippySites\Helpers\Loader;
 
-  public static function version() 
-  { 
-    return '1.0.0-alpha'; 
-  } 
+class Core {
+ 
+ var $location;
+ var $language;
+ var $plugin_dir = '';
+ var $plugin_url = '';
+ var $pro_sites = array();
+ var $level = array();
+	var $checkout_processed = false;
+
+	var $tcpdf = array(); //Array for PDF settings
+ 
+ function __construct() {
+  //setup our variables
+  $this->init_vars;
+  
+  
+  
+ }
+
+ public static function version() 
+ { 
+  return '1.0.0-alpha'; 
+ } 
   
 }
